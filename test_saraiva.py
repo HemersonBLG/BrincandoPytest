1c0133be18d958202e98a87679533998316cf574
Hemerson Boaventura de Lima Gomes <hemerson.blg@gmail.com>
2019-03-30 01:46:02 -0300

    Opa
    Parece que finalmente consegui haha, a parte de cadastro está ok, posso utilizar o método de pegar as informações para outros programas

--- test_saraiva.py
+++ test_saraiva.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
-import time
 import pytest
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+import time
 
 @pytest.fixture()
 def test_setup():
     global driver
     driver = webdriver.Chrome('C:/Users/hemer/Desktop/Automation/chromedriver')
-    driver.implicitly_wait(10)
     driver.maximize_window()
     yield
     driver.close()
@@ -16,7 +17,7 @@ def test_setup():
 
 
 def test_geraPessoa(test_setup):
-    global nome, cpf, email, celular, cep, numEnd
+    global nome, cpf, email, celular, cep, numEnd, dtNsc
     driver.get('https://www.4devs.com.br/gerador_de_pessoas')
     driver.find_element_by_id('bt_gerar_pessoa').click()
     time.sleep(3)
@@ -32,13 +33,29 @@ def test_geraPessoa(test_setup):
     assert cep != ''
     numEnd = driver.find_element_by_id('numero').text
     assert numEnd != ''
+    dtNsc = driver.find_element_by_id('data_nasc').text
+    assert dtNsc != ''
 
 #Site Saraiva
 def test_cadastraPessoa(test_setup):
+    nome_d = nome.split()
+    nome_p = nome_d[0]
+    nome_s = nome_d[1]
     driver.get('https://www.saraiva.com.br/customer/account/create')
-    driver.find_element_by_id('firstname').send_keys(nome)
-
-'''def test_teardown():
-    driver.close()
-    driver.quit()
-    print('Teste completo')'''
+    EC.text_to_be_present_in_element((By.TAG_NAME, 'h1'), 'Identificação')
+    driver.find_element_by_id('firstname_pf').send_keys(nome_p)
+    driver.find_element_by_id('lastname').send_keys(nome_s)
+    driver.find_element_by_id('email_address_pf').send_keys(email)
+    driver.find_element_by_id('password').send_keys('AbC123')
+    driver.find_element_by_id('confirmation').send_keys('AbC123')
+    driver.find_element_by_id('cpf-number').send_keys(cpf)
+    driver.find_element_by_id('gender').click()
+    #selecionando sexo, 1 = Masculino, 2 = Feminino
+    sexo = Select(driver.find_element_by_id('gender'))
+    sexo.select_by_value('1')
+    driver.find_element_by_id('fulldob').send_keys(dtNsc)
+    driver.find_element_by_id('celular_pf').send_keys(celular)
+    driver.find_element_by_id('zip').send_keys(cep)
+    time.sleep(2)
+    driver.find_element_by_id('address:numero_endereco').send_keys(numEnd)
+    driver.find_element_by_id('telephone_pf').send_keys(celular)
