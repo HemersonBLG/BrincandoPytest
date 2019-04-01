5e2f8c3885c8a5f1a7a1bfe03d2938cadae5705e
Hemerson Boaventura de Lima Gomes <hemerson.blg@gmail.com>
2019-04-01 09:58:20 -0300

    Update test_saraiva.py

--- test_saraiva.py
+++ test_saraiva.py
@@ -38,13 +38,10 @@ def test_geraPessoa(test_setup):
 
 #Site Saraiva
 def test_cadastraPessoa(test_setup):
-    nome_d = nome.split()
-    nome_p = nome_d[0]
-    nome_s = nome_d[1]
     driver.get('https://www.saraiva.com.br/customer/account/create')
     EC.text_to_be_present_in_element((By.TAG_NAME, 'h1'), 'Identificação')
-    driver.find_element_by_id('firstname_pf').send_keys(nome_p)
-    driver.find_element_by_id('lastname').send_keys(nome_s)
+    driver.find_element_by_id('firstname_pf').send_keys(nome.split()[0])
+    driver.find_element_by_id('lastname').send_keys(nome.split()[2])
     driver.find_element_by_id('email_address_pf').send_keys(email)
     driver.find_element_by_id('password').send_keys('AbC123')
     driver.find_element_by_id('confirmation').send_keys('AbC123')
